56ac6acef5a64a0a1a559bd0b5bc2e2bcb37bc36
raximboyevagulchiroy-tech <raximboyevagulchiroy9@gmail.com>
2025-11-13 17:36:31 +0500

    python

---
+++ homework.py
@@ -0,0 +1,58 @@
+# # 1
+# def yil(ism,yosh):
+#     tugilgan_yil = 2025 - int(yosh)
+#     print(f"Hurmatli {ism.title()} siz {tugilgan_yil}-yilda tug'ilgansiz")
+#
+#
+# yil("guli" , 19)
+
+# # 2
+# def son(a):
+#     print(f"Kiritilgan sonning kvadrati ={a**2}\n"
+#           f"Kiritilgan sonning kubi ={a**3}")
+#
+# son(10)
+#
+
+# # 3
+# def son(a):
+#     if a % 2 == 0:
+#         print("Berilgan son juft")
+#     else:
+#         print("Berilgan son toq")
+#
+# son(20)
+
+# # 4
+# def son(a,b):
+#     if a>b:
+#         print(a)
+#     elif b>a:
+#         print(b)
+#     else:
+#         print("Sonlar teng")
+#
+# son(13,23)
+
+# # 5
+# def son(x,y):
+#     A = x**y
+#     print("x^y =",A)
+#
+# son(12,4)
+
+# # 6
+# def son(x,y=2):
+#     A = x**y
+#     print("x^y =",A)
+#
+# son(10)
+
+# 7
+# def bolinish_alomatlari(a):
+#     for i in range(2,11):
+#         if  a % i == 0:
+#          print(f"{a} {i} ga qoldiqsiz bo'linadi")
+#
+# bolinish_alomatlari(70)
+
